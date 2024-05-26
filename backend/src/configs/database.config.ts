1cdd37dfaaed8b05f56f1645c0ee472f39efd367
Jose Avila <javilaa5@miumg.edu.gt>
2024-05-26 13:26:57 -0600

    Coneccion DB

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -1,8 +1,10 @@
 import postgres from "postgres";
 
-// Conexion a la base de datos local ejercicio1
-
-// Localhost
-const dbConnect = postgres('postgres://admin:fmCgLTDAWmeV5zOMfbNG9Pl2WLQZbWcc@dpg-cp8vgf5ds78s73cacr10-a.oregon-postgres.render.com\n/ejercicio1');
+// Asegúrate de que la conexión a la base de datos incluye SSL
+const dbConnect = postgres('postgres://admin:fmCgLTDAWmeV5zOMfbNG9Pl2WLQZbWcc@dpg-cp8vgf5ds78s73cacr10-a.oregon-postgres.render.com/ejercicio1', {
+    ssl: {
+        rejectUnauthorized: false // Esta opción permite conexiones a servidores con certificados autofirmados. En producción, considera usar `true`.
+    }
+});
 
 export default dbConnect;
